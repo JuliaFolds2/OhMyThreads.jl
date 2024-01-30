832002aca9d9b3ea7e4631e3d16d570294db17d8
Mason Protter <mason.protter@icloud.com>
2024-01-30 20:14:51 +0100

    Support interactive threadpool, and multiple array arguments (#14)
    * add taskid function, update readme
    
    * bump version
    
    * support threadpools, and multiple array arguments
    
    * drop 1.6
    
    * fix docstring
    
    * add docstring info about `:interactive`
    
    * test with interactive threadpool

--- src/implementation.jl
+++ src/implementation.jl
@@ -9,77 +9,96 @@ using Base.Threads: nthreads, @threads
 
 using BangBang: BangBang, append!!
 
-function tmapreduce(f, op, A;
+function tmapreduce(f, op, Arrs...;
     nchunks::Int=nthreads(),
     split::Symbol=:batch,
     schedule::Symbol=:dynamic,
     outputtype::Type=Any,
     kwargs...)
-    if schedule === :dynamic
-        _tmapreduce(f, op, A, outputtype, nchunks, split; kwargs...)
+    if schedule === :dynamic 
+        _tmapreduce(f, op, Arrs, outputtype, nchunks, split, :default; kwargs...)
+    elseif schedule === :interactive 
+        _tmapreduce(f, op, Arrs, outputtype, nchunks, split, :interactive; kwargs...)
     elseif schedule === :static
-        _tmapreduce_static(f, op, A, outputtype, nchunks, split; kwargs...)
+        _tmapreduce_static(f, op, Arrs, outputtype, nchunks, split; kwargs...)
     else
         schedule_err(schedule)
     end
 end
 @noinline schedule_err(s) = error(ArgumentError("Invalid schedule option: $s, expected :dynamic or :static."))
 
-treducemap(op, f, A; kwargs...) = tmapreduce(f, op, A; kwargs...)
+treducemap(op, f, A...; kwargs...) = tmapreduce(f, op, A...; kwargs...)
 
-function _tmapreduce(f, op, A, ::Type{OutputType}, nchunks, split=:batch; kwargs...)::OutputType where {OutputType}
-    tasks = map(chunks(A; n=nchunks, split)) do inds
-        @spawn mapreduce(f, op, @view(A[inds]); kwargs...)
+function _tmapreduce(f, op, Arrs, ::Type{OutputType}, nchunks, split, schedule; kwargs...)::OutputType where {OutputType}
+    check_all_have_same_indices(Arrs)
+    tasks = map(chunks(first(Arrs); n=nchunks, split)) do inds
+        args = map(A -> A[inds], Arrs)
+        @spawn schedule mapreduce(f, op, args...; kwargs...)
     end
     mapreduce(fetch, op, tasks)
 end
 
-function _tmapreduce_static(f, op, A, ::Type{OutputType}, nchunks, split; kwargs...) where {OutputType}
+function _tmapreduce_static(f, op, Arrs, ::Type{OutputType}, nchunks, split; kwargs...) where {OutputType}
     nt = nthreads()
+    check_all_have_same_indices(Arrs)
     if nchunks > nt
         # We could implement strategies, like round-robin, in the future
         throw(ArgumentError("We currently only support `nchunks <= nthreads()` for static scheduling."))
     end
-    tasks = map(enumerate(chunks(A; n=nchunks, split))) do (c, inds)
+    tasks = map(enumerate(chunks(first(Arrs); n=nchunks, split))) do (c, inds)
         tid = @inbounds nthtid(c)
-        @spawnat tid mapreduce(f, op, @view(A[inds]); kwargs...)
+        args = map(A -> A[inds], Arrs)
+        @spawnat tid mapreduce(f, op, args...; kwargs...)
     end
     mapreduce(fetch, op, tasks)
 end
 
+
+check_all_have_same_indices(Arrs) = let A = first(Arrs), Arrs = Arrs[2:end]
+    if !all(B -> eachindex(A) == eachindex(B), Arrs)
+        error("The indices of the input arrays must match the indices of the output array.")
+    end
+end 
+
 #-------------------------------------------------------------
 
-function treduce(op, A; kwargs...)
-    tmapreduce(identity, op, A; kwargs...)
+function treduce(op, A...; kwargs...)
+    tmapreduce(identity, op, A...; kwargs...)
 end
 
 #-------------------------------------------------------------
 
-function tforeach(f, A::AbstractArray; kwargs...)::Nothing
-    tmapreduce(f, (l, r) -> l, A; kwargs..., init=nothing, outputtype=Nothing)
+function tforeach(f, A...; kwargs...)::Nothing
+    tmapreduce(f, (l, r) -> l, A...; kwargs..., init=nothing, outputtype=Nothing)
 end
 
 #-------------------------------------------------------------
 
-function tmap(f, ::Type{T}, A::AbstractArray; kwargs...) where {T}
-    tmap!(f, similar(A, T), A; kwargs...)
+function tmap(f, ::Type{T}, A::AbstractArray, _Arrs::AbstractArray...; kwargs...) where {T}
+    Arrs = (A, _Arrs...)
+    tmap!(f, similar(A, T), Arrs...; kwargs...)
 end
 
-function tmap(f, A; nchunks::Int= 2*nthreads(), kwargs...)
+function tmap(f, A::AbstractArray, _Arrs::AbstractArray...; nchunks::Int=nthreads(), kwargs...)
+    Arrs = (A, _Arrs...)
+    check_all_have_same_indices(Arrs)
     the_chunks = collect(chunks(A; n=nchunks))
     # It's vital that we force split=:batch here because we're not doing a commutative operation!
     v = tmapreduce(append!!, the_chunks; kwargs...,  nchunks, split=:batch) do inds
-        map(f, @view A[inds])
+        args = map(A -> @view(A[inds]), Arrs)
+        map(f, args...)
     end
     reshape(v, size(A)...)
 end
 
-@propagate_inbounds function tmap!(f, out, A::AbstractArray; kwargs...)
-    @boundscheck eachindex(out) == eachindex(A) || error("The indices of the input array must match the indices of the output array.")
+@propagate_inbounds function tmap!(f, out, A::AbstractArray, _Arrs::AbstractArray...; kwargs...)
+    Arrs = (A, _Arrs...)
+    @boundscheck check_all_have_same_indices((out, Arrs...))
     # It's vital that we force split=:batch here because we're not doing a commutative operation!
-    tforeach(eachindex(A); kwargs..., split=:batch) do i
-        fAi = f(@inbounds A[i])
-        out[i] = fAi
+    tforeach(eachindex(out); kwargs..., split=:batch) do i
+        args = map(A -> @inbounds(A[i]), Arrs)
+        res = f(args...)
+        out[i] = res
     end
     out
 end
