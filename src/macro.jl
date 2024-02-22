bdb31c9b2903b507fc6e472635089f54850c7a78
Carsten Bauer <crstnbr@gmail.com>
2024-02-22 13:58:39 +0100

    reduce -> reducer; support begin end block for tasklocal macro

--- src/macro.jl
+++ src/macro.jl
@@ -8,6 +8,41 @@ function _kwarg_to_tuple(ex)
     (name, val)
 end
 
+function _tasklocal_assign_to_exprs(ex)
+    if ex.head != :(=)
+        throw(ErrorException("Wrong usage of @tasklocal. Expected assignment, e.g. `A::Matrix{Float} = rand(2,2)`."))
+    end
+    left_ex = ex.args[1]
+    if left_ex isa Symbol || left_ex.head != :(::)
+        throw(ErrorException("Wrong usage of @tasklocal. Expected typed assignment, e.g. `A::Matrix{Float} = rand(2,2)`."))
+    end
+    tls_sym = left_ex.args[1]
+    tls_type = left_ex.args[2]
+    tls_def = ex.args[2]
+    tls_storage = gensym()
+    tlsinit = :($(tls_storage) = OhMyThreads.TaskLocalValue{$tls_type}(() -> $(tls_def)))
+    tlsblock = :($(tls_sym) = $(tls_storage)[])
+    return tlsinit, tlsblock
+end
+
+function _unfold_tasklocal_block(ex)
+    if ex.head == :(=)
+        tlsinit, tlsblock = _tasklocal_assign_to_exprs(ex)
+    elseif ex.head == :block
+        tlsexprs = filter(x -> x isa Expr, ex.args) # skip LineNumberNode
+        tlsinit = quote end
+        tlsblock = quote end
+        for x in tlsexprs
+            tlsi, tlsb = _tasklocal_assign_to_exprs(x)
+            push!(tlsinit.args, tlsi)
+            push!(tlsblock.args, tlsb)
+        end
+    else
+        throw(ErrorException("Wrong usage of @tasklocal. You must either provide a typed assignment or multiple typed assignments in a `begin ... end` block."))
+    end
+    return tlsinit, tlsblock
+end
+
 macro threaded(args...)
     forex = last(args)
     kwexs = args[begin:(end - 1)]
@@ -25,7 +60,7 @@ macro threaded(args...)
             else
                 scheduler = val
             end
-        elseif name == :reduce
+        elseif name == :reducer
             reducer = val
         else
             throw(ArgumentError("Unknown keyword argument: $name"))
@@ -41,32 +76,14 @@ macro threaded(args...)
         forbody = forex.args[2]
     end
 
-    lbi = findfirst(forbody.args) do arg
+    tlsinit = nothing
+    tlsblock = nothing
+    tlsidx = findfirst(forbody.args) do arg
         arg isa Expr && arg.head == :macrocall && arg.args[1] == Symbol("@tasklocal")
     end
-    if !isnothing(lbi)
-        assignment_ex = forbody.args[lbi].args[3]
-        if assignment_ex.head != :(=)
-            throw(ErrorException("Wrong usage of @tasklocal. Expected assignment, e.g. `A::Matrix{Float} = rand(2,2)`."))
-        end
-        left_ex = assignment_ex.args[1]
-        if left_ex isa Symbol || left_ex.head != :(::)
-            throw(ErrorException("Wrong usage of @tasklocal. Expected typed assignment, e.g. `A::Matrix{Float} = rand(2,2)`."))
-        end
-        tls_sym = left_ex.args[1]
-        tls_type = left_ex.args[2]
-        tls_def = assignment_ex.args[2]
-        tls_storage = gensym()
-        tlsinit = quote
-            $(tls_storage) = OhMyThreads.TaskLocalValue{$tls_type}(() -> $(tls_def))
-        end
-        tlsblock = quote
-            $(tls_sym) = $(tls_storage)[]
-        end
-        deleteat!(forbody.args, lbi)
-    else
-        tlsinit = nothing
-        tlsblock = nothing
+    if !isnothing(tlsidx)
+        tlsinit, tlsblock = _unfold_tasklocal_block(forbody.args[tlsidx].args[3])
+        deleteat!(forbody.args, tlsidx)
     end
 
     q = if isnothing(reducer)
