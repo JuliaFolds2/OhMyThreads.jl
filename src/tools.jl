e16de1a7267a00dd4801b0bee6f4c59845376cc5
Carsten Bauer <crstnbr@gmail.com>
2024-03-21 17:11:27 +0100

    add SimpleBarrier to tools

--- src/tools.jl
+++ src/tools.jl
@@ -82,4 +82,52 @@ function reset!(s::OnlyOneRegion)
     nothing
 end
 
+"""
+SimpleBarrier(n::Integer)
+
+Simple reusable barrier for `n` parallel tasks.
+
+Given `b = SimpleBarrier(n)` and `n` parallel tasks, each task that calls
+`wait(b)` will block until the other `n-1` tasks have called `wait(b)` as well.
+
+## Example
+```
+n = nthreads()
+barrier = SimpleBarrier(n)
+@sync for i in 1:n
+    @spawn begin
+        println("A")
+        wait(barrier) # synchronize all tasks
+        println("B")
+        wait(barrier) # synchronize all tasks (reusable)
+        println("C")
+    end
+end
+```
+"""
+mutable struct SimpleBarrier
+    const n::Int64
+    const c::Threads.Condition
+    cnt::Int64
+
+    function SimpleBarrier(n::Integer)
+        new(n, Threads.Condition(), 0)
+    end
+end
+
+function Base.wait(b::SimpleBarrier)
+    lock(b.c)
+    try
+        b.cnt += 1
+        if b.cnt == b.n
+            b.cnt = 0
+            notify(b.c)
+        else
+            wait(b.c)
+        end
+    finally
+        unlock(b.c)
+    end
+end
+
 end # Tools
