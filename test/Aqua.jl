7643d74a898d2c3633159284b8e2e1e2f3363e7a
MasonProtter <mason.protter@icloud.com>
2025-03-15 16:08:25 +0100

    fix detection of ScopedValues

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -1,11 +1,12 @@
 using Aqua
 
 @testset "Aqua.jl" begin
+  ignore = isdefined(Base, :ScopedValues) ? [:ScopedValues] : []
   Aqua.test_all(
     OhMyThreads;
     # ambiguities=(exclude=[SomePackage.some_function], broken=true),
-    # stale_deps=(ignore=[:SomePackage],),
-    deps_compat=(ignore=[:Test],),
+    stale_deps=(ignore=[:ScopedValues],),
+    deps_compat=(;ignore,),
     # piracies=false,
     persistent_tasks=false,
   )
